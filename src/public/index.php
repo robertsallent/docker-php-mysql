57ab357a49a627f69b2b18de5d7a8410fc1b2d68
Robert Sallent <robertsallent@gmail.com>
2024-03-04 12:59:08 +0100

    v0.3.0
    - Actualizados estilos.
    - Eliminado php.ini
    - Movidos ejemplos de php.ini a la carpeta config.

--- src/public/index.php
+++ src/public/index.php
@@ -2,29 +2,43 @@
 <html lang="es">
 	<head>
 		<title>PHP-MySQL Docker environment</title>
+		
 		<meta charset="UTF-8">
+		<meta description="Página de prueba del ejemplo de Docker para clase">
+		<meta author="Robert Sallent">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">	
+		
+		<link rel="shortcut icon" href="/images/small.png" type="image/png">	
 		<link rel="stylesheet" type="text/css" href="https://robertsallent.com/css/generic.css">
 	</head>
 	<body>
-		<h1>It Works!</h1>
-		<h2>Apache con php ha arrancado correctamente</h2>
+		<header class="flex-container">
+			<figure id="logo" class="flex1">
+				<img src="images/logo.png">
+			</figure>
+    		<hgroup class="flex5">
+        		<h1>It Works!</h1>
+        		<p>Apache con php ha arrancado correctamente</p>
+    		</hgroup>
+		</header>
 		<p>Si puedes ver esto, significa que el servidor de Apache con php ha arrancado correctamente
 		dentro de su contenedor de Docker.</p>
 		
 		<p>Ahora debes asegurarte que MySQL también ha arrancado sin problemas y está listo, para ello puedes crear
-		una nueva conexión con Workbench con los siguientes datos:</p>
+		una nueva conexión con los siguientes datos:</p>
 		<ul>
 			<li>host: <b>localhost</b></li>
 			<li>port: <b>3306</b></li>
 			<li>user: <b>root</b></li>
 			<li>pass: <b>dbpass</b></li>
 		</ul>
-		<p>Si también funciona, ya estás preparado para trabajar.</p>
+		<p>Si también funciona, ya estás preparado para trabajar, los detalles de la versión de 
+		   <b>php</b> instalada los encontrarás en <a href="phpinfo.php">este enlace</a>.</p>
 		
-		<p>Ejemplo creado por <a href="https://robertsallent.com">Robert Sallent</a> para los cursos que imparte.</p>
-		
-		<p>Aquí se muestra la información detallada de php con un <i>phpinfo()</i>:</p>
-		<?php phpinfo() ?>
+		<footer>
+			<p>Ejemplo creado por <a href="https://robertsallent.com" rel="author">Robert Sallent</a> para los cursos que imparte.</p>
+			<p>Más información en <a href="https://www.linkedin.com/in/robert-sallent-l%C3%B3pez-4187a866/">LinkedIn</a>.</p>
+		</footer>
 	</body>
 
 </html>
